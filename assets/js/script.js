fa9eeea539585db3ca471d6e7c15409d2a12b43f
Goat <dpajbk@gmail.com>
2022-10-10 15:01:16 -0400

    html adjusted, pseudocode comments added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,21 @@
 // dependencies
+var day1= $('#day1')
+var day2= $('#day2')
+var day3= $('#day3')
+var day4= $('#day4')
+var day5= $('#day5')
 
+// DATA
+
+// functions
+
+// retrieve location data on form/button click
+
+// convert location data into coordinates
+
+// call API with provided coordinates
 function getAPI() {
-    var requestURL= "https://api.openweathermap.org/data/2.5/forecast?lat=41.053429&lon=73.538734&appid=b442f7cbd3dba47d0df28083d882bce6"
+    var requestURL= "https://api.openweathermap.org/data/2.5/forecast?lat=41.053429&lon=-73.538734&units=imperial&appid=b442f7cbd3dba47d0df28083d882bce6"
 
     fetch(requestURL)
         .then(function (response) {
@@ -11,5 +25,7 @@ function getAPI() {
             console.log(data)
         })
 }
+// write weather from API to each card
 
 getAPI()
+
